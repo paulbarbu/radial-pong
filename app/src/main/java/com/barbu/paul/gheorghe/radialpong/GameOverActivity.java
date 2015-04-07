006dce89e3b53bcf026e23af49f18df80f8a8787
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2015-04-07 20:28:54 +0300

    get the nickname on the game over screen

--- app/src/main/java/com/barbu/paul/gheorghe/radialpong/GameOverActivity.java
+++ app/src/main/java/com/barbu/paul/gheorghe/radialpong/GameOverActivity.java
@@ -1,14 +1,33 @@
 package com.barbu.paul.gheorghe.radialpong;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class GameOverActivity extends Activity {
+    private static final String TAG = MenuActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+    }
+
+    public void submit(View view){
+        EditText nicknameText = (EditText) findViewById(R.id.nickname);
+        String nickname = nicknameText.getText().toString();
+
+        if(nickname.length() <= 0)
+        {
+            Log.d(TAG, "Submitted invalid nick");
+        }
+        else {
+            Log.d(TAG, "Submitted nick: " + nickname);
+        }
+
     }
 }
